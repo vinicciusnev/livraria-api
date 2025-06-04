2e3fae97be5992fa8660ea9074f9baffd6ff7a6d
vinicciusnev <xxnashflows@hotmail.com>
2025-06-04 20:43:39 -0300

    feat: Implementando DTO's de livro e autor

---
+++ app/schemas/autor.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from typing import Optional, List
+from app.schemas.livro import LivroOut
+
+
+class AutorBase(BaseModel):
+    nome: str
+    nacionalidade: Optional[str]
+
+
+class AutorCreate(AutorBase):
+    pass
+
+
+class AutorUpdate(BaseModel):
+    nome: Optional[str] = None
+    nacionalidade: Optional[str] = None
+
+
+class AutorOut(AutorBase):
+    id: int
+    livros: List[LivroOut] = []
+
+    model_config = {
+        "from_attributes": True
+    }
---
+++ app/schemas/livro.py
@@ -0,0 +1,26 @@
+from decimal import Decimal
+
+from pydantic import BaseModel
+from typing import Optional
+
+class LivroBase(BaseModel):
+    titulo: str
+    ano: Optional[int]
+    preco: Optional[Decimal]
+    autor_id: int
+
+class LivroCreate(LivroBase):
+    pass
+
+class LivroUpdate(BaseModel):
+    titulo: Optional[str] = None
+    ano: Optional[int] = None
+    preco: Optional[Decimal] = None
+    autor_id: Optional[int] = None
+
+class LivroOut(LivroBase):
+    id: int
+
+    model_config = {
+        "from_attributes": True
+    }
