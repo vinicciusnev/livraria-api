44ef11c18cb6a619bbe91c3146b26266e82aeea7
vinicciusnev <xxnashflows@hotmail.com>
2025-06-04 20:42:04 -0300

    chore: arquivo db.sql com os ddls das tabelas do banco

---
+++ db.sql
@@ -0,0 +1,19 @@
+-- Cria o schema (se não existir)
+CREATE SCHEMA IF NOT EXISTS livraria;
+
+-- Tabela de Autores no schema livraria
+CREATE TABLE livraria.autores (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    nacionalidade VARCHAR(255)
+);
+
+-- Tabela de Livros no schema livraria
+CREATE TABLE livraria.livros (
+    id SERIAL PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    ano INTEGER,
+    preco INTEGER,
+    autor_id INTEGER,
+    CONSTRAINT fk_autor FOREIGN KEY (autor_id) REFERENCES livraria.autores(id) ON DELETE SET NULL
+);
